4b12c3d0b6012cdf653e5c848be339ea2bd4f7cf
yshao3 <syj_19930514@hotmail.com>
2017-05-01 23:29:39 -0400

    midtermpre

--- code/inpainting.m
+++ code/inpainting.m
@@ -1,7 +1,7 @@
 % % fill output function
-patch = im2double(imread('../data/jelly.jpg'));
+% patch = im2double(imread('../data/jelly.jpg'));
 % % patch = patch.*mask;
-% patch = im2double(imread('../data/basket_png_flipped.png'));
+patch = im2double(imread('../data/basket_png_flipped.png'));
 % % patch = im2double(imread('in/strawberries2.jpg'));
 % size(patch)
 % mask = ones(size(patch,1),size(patch,2));
@@ -36,22 +36,30 @@ outputImagePatchLabels = zeros(OUT_IMG_HEIGHT, OUT_IMG_WIDTH);
 %% Place the first patch
 % simply copy the entire patch onto the output image
 outputImage(1:size(patch, 1), 1:size(patch, 2), :) = patch;
-figure(1)
-imshow(outputImage);
+% figure(1)
+% imshow(outputImage);
 % update the label image so that the updated values have a label of "1"
 outputImagePatchLabels(1:size(patch, 1), 1:size(patch, 2)) = ones(size(patch, 1), size(patch, 2));
 % outputImagePatchLabels(130:160,150:190) = zeros(31,41);
 % while any(outputlabel == 0)
 label = 1;
 s = 9;
-while sum(sum(outputImagePatchLabels==0))>0
+% while sum(sum(outputImagePatchLabels==0))>0
+index = 1;
+for i =1:5
     label = label+1;
-    [toFill,Hp,rows,cols] = findlocation(outputImage,outputImagePatchLabels,21);
+    [toFill,Hp,rows,cols] = findlocation(outputImage,outputImagePatchLabels,51);
     toFill = toFill';
-    imshow(toFill);
     targetpatch = outputImage(rows(1):rows(1)+size(toFill,1)-1....
                             ,cols(1):cols(1)+size(toFill,2)-1,:);
+                  
+    figure(1)
+    hold on;
+    subplot(5,2,index); imshow(targetpatch);
+    index = index+1;
     [mostmatchPatch, shifted] = MatchingSimilarPatches(toFill,targetpatch,patchbackup,0,1);
+    subplot(5,2,index); imshow(mostmatchPatch);
+    index = index+1;
     offset = [max(1, rows(1)), max(1, cols(1))];
     patchOnBackground = zeros(size(outputImage));
     patchOnBackgroundMask = zeros(size(outputImage, 1), size(outputImage, 2));
@@ -66,8 +74,8 @@ while sum(sum(outputImagePatchLabels==0))>0
     outputImage = alphaMask(outputImage, mostmatchPatch, offset, mask);
     outputImagePatchLabels = alphaMask(outputImagePatchLabels, mask*label, offset, mask);
 end 
-figure(2)
-imshow(outputImage);
-figure(3)
-imagesc(outputImagePatchLabels);
+% figure(2)
+% imshow(outputImage);
+% figure(3)
+% imagesc(outputImagePatchLabels);
 % end
